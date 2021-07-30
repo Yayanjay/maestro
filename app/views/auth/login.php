81a58792794aece38e8cec5a1c6a0d49f665ba17
Yayanjay <yayanabdillah01@gmail.com>
2021-07-30 19:07:07 +0700

    delete unused js

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -8,21 +8,9 @@
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="<?= BASEURL ?>/images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/css/util.css">
 	<link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/css/main.css">
@@ -73,27 +61,6 @@
 			</div>
 		</div>
 	</div>
-	
-	
-
-	
-	
-<!--===============================================================================================-->
-	<script src="<?= BASEURL ?>/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="<?= BASEURL ?>/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="<?= BASEURL ?>/vendor/bootstrap/js/popper.js"></script>
-	<script src="<?= BASEURL ?>/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="<?= BASEURL ?>/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="<?= BASEURL ?>/vendor/daterangepicker/moment.min.js"></script>
-	<script src="<?= BASEURL ?>/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="<?= BASEURL ?>/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
 	<script src="<?= BASEURL ?>/js/main.js"></script>
-
 </body>
 </html>
